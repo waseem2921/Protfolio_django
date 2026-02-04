c4233c0db64f5ecec3b6aef53ed0246b56da0d2a
waseem2921 <waseemferozkhan2921@gmail.com>
2026-02-04 19:56:34 +0530

    Final commit message:

--- Protfolio/Protfolio/settings.py
+++ Protfolio/Protfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'Protfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'content',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'FEROZKHAN',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'Trusted_Connection': 'yes',
+        },
     }
 }
 
@@ -117,9 +125,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
+MEDIA_ROOT='/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
